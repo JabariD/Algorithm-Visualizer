2f2f2a367679af37fa1840766ea0a9a8fa23fb2f
Payton Dennis <dennispayton15@gmail.com>
2020-11-03 18:59:43 -0600

    Add 404 error route

---
+++ src/components/ErrorRoute.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ErrorRoute() {
+    return (
+        <div>
+            <h1>Could not find the page you requested</h1>
+        </div>
+    )
+}
