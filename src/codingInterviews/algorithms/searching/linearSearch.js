a65724678219178fe27b5a2892793dc22589ada0
Payton Dennis <dennispayton15@gmail.com>
2020-11-03 19:00:39 -0600

    Add LinearSearch Component

---
+++ src/codingInterviews/algorithms/searching/linearSearch.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+// Runs the linear search algorithm
+export class linearSearch extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default linearSearch
